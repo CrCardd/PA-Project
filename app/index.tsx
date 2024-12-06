36923ce00fef817391674ae93140661a4cf5c151
CrCardd <cris.rc377@gmail.com>
2024-12-06 14:49:11 -0300

    a

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,4 @@
 import { StyleSheet, View, Image, TouchableOpacity, Text } from 'react-native';
-import { Link, router } from "expo-router";
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function HomeScreen() {
@@ -8,18 +7,18 @@ export default function HomeScreen() {
     // router.push("/(login)");
   };
 
-
-
   return (
     <View style={{flex: 1}}>
         <View style={styles.background}>
-          {/* <TouchableOpacity style={styles.button} onPress={onPress}>
-            <Text style={styles.btnText}>ENTRAR</Text>
-          </TouchableOpacity> */}
-          <Image src={require('@/assets/images/login_bg.png')}/>
+          <Image 
+            source={require('@/assets/images/login_bg.png')} 
+            height={10000}
+            width={10000}
+            style={styles.image}
+          />
         </View>
         <LinearGradient
-            colors={['rgba(8,9,0,0.98)', 'transparent']}
+            colors={['rgba(8,9,0,1)', 'transparent']}
             start={{ x: 0, y: 1 }} 
             end={{ x: 0, y: 0 }}   
             style={styles.gradient}  
@@ -30,26 +29,17 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
     background: {
-        // ...StyleSheet.absoluteFillObject,
-        // width: '100%',
-        // height: '100%',
-        
         flex: 1,
-        backgroundColor: '#d1f4a9'
     },
     gradient: {
         position: 'absolute',
         bottom: 0,
         width: '100%',
-        height: '50%',
-      },
-
-
-    button: {
-      backgroundColor: "#090909",
+        height: '80%',
     },
-    btnText: {
-      fontFamily: "Inter",
-      color: "#ffffff",
+    image: {
+        width: '100%',
+        height: '100%',  
+        resizeMode: 'cover',
     },
 });
